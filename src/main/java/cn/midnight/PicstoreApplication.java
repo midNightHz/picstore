e53f62fd79af945295dc72ee266724876a5ad787
chenj5618 <76765168@qq.com>
2019-04-24 10:32:56 +0800

    1

---
+++ src/main/java/cn/midnight/PicstoreApplication.java
@@ -0,0 +1,13 @@
+package cn.midnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicstoreApplication.class, args);
+	}
+
+}
